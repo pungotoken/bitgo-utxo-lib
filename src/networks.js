b0d3d2d6ab6fc1e1f82cbf7c28f33f7c4b65f3be
TheTrunk <tadeas.kmenta@zel.cash>
2018-12-19 11:10:22 +0100

    zero,snowgem

--- src/networks.js
+++ src/networks.js
@@ -188,5 +188,45 @@ module.exports = {
       4: 0x76b809bb
     },
     coin: coins.ZEC
-  }
+  },
+  zero: {
+    messagePrefix: '\x18Zero Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x1cb8,
+    scriptHash: 0x1cbd,
+    wif: 0x80,
+    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
+    // dummy value.
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5ba81b19,
+      4: 0x76b809bb
+    },
+    coin: coins.ZEC
+  },
+  snowgem: {
+    messagePrefix: '\x18Snowgem Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x1c28,
+    scriptHash: 0x1c2d,
+    wif: 0x80,
+    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
+    // dummy value.
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5ba81b19,
+      4: 0x76b809bb
+    },
+    coin: coins.ZEC
+  },
 }
