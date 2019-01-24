2e3b9af81fe5d26745b4c4c907cfe3ff8a2c61c4
TheTrunk <tadeas.kmenta@zel.cash>
2019-01-24 23:33:21 +0100

    implement commercium

--- src/networks.js
+++ src/networks.js
@@ -229,4 +229,24 @@ module.exports = {
     },
     coin: coins.ZEC
   },
+  commercium: {
+    messagePrefix: '\x18Commercium Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x1c,
+    scriptHash: 0x33,
+    wif: 0x8c,
+    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
+    // dummy value.
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5ba81b19,
+      4: 0x76b809bb
+    },
+    coin: coins.ZEC
+  },
 }
