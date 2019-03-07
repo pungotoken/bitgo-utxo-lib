5c26fbc425f8699eeaf6e893ebc2878372b1b665
TheTrunk <tadeas.kmenta@zel.cash>
2019-03-07 13:27:15 +0100

    safe

--- src/networks.js
+++ src/networks.js
@@ -179,6 +179,27 @@ module.exports = {
     coin: coins.ZEC,
     hashFunctions: hashFunctions
   },
+  safecoin: {
+    messagePrefix: '\x18Safecoin Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21f,
+      private: 0x0488ade5
+    },
+    pubKeyHash: 0x3d,
+    scriptHash: 0x56,
+    wif: 0xbd,
+    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
+    // dummy value.
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5ba81b19,
+      4: 0x76b809bb
+    },
+    coin: coins.ZEC,
+    hashFunctions: hashFunctions
+  },
   bithereum: {
     messagePrefix: '\x18Bithereum Signed Message:\n',
     bech32: 'bth',
