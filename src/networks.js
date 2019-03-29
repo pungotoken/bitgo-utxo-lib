4608d5ab460657ee4a8291725ff6b46e1089b508
TheTrunk <tadeas.kmenta@zel.cash>
2019-03-29 22:54:42 +0100

    btcz

--- src/networks.js
+++ src/networks.js
@@ -379,4 +379,25 @@ module.exports = {
     coin: coins.BTC,
     hashFunctions: hashFunctions
   },
+  bitcoinz: {
+    messagePrefix: '\x18BitcoinZ Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x1cb8,
+    scriptHash: 0x1cbd,
+    wif: 0x80,
+    // This parameter was introduced in version 3 to allow soft forks, for version 1 and 2 transactions we add a
+    // dummy value.
+    consensusBranchId: {
+      1: 0x00,
+      2: 0x00,
+      3: 0x5ba81b19,
+      4: 0x76b809bb
+    },
+    coin: coins.ZEC,
+    hashFunctions: hashFunctions
+  },
 }
